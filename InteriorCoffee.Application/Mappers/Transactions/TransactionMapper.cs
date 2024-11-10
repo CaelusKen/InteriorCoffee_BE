630faab9d1bc5a9afac0315805903901fe1d937d
hntduong12345 <hntduong2003@gmail.com>
2024-11-10 19:06:44 +0700

    chore: add default create transaction date

--- InteriorCoffee.Application/Mappers/Transactions/TransactionMapper.cs
+++ InteriorCoffee.Application/Mappers/Transactions/TransactionMapper.cs
@@ -18,6 +18,7 @@ namespace InteriorCoffee.Application.Mappers.Transactions
             CreateMap<CreateTransactionDTO, Transaction>()
                 .ForMember(des => des._id, src => src.MapFrom(src => ObjectId.GenerateNewId().ToString()))
                 .ForMember(des => des.UpdatedDate, src => src.MapFrom(src => DateTime.UtcNow))
+                .ForMember(des => des.TransactionDate, src => src.MapFrom(src => DateTime.UtcNow))
                 .ForMember(des => des.Status, src => src.MapFrom(src => TransactionStatusEnum.PENDING.ToString()));
         }
     }

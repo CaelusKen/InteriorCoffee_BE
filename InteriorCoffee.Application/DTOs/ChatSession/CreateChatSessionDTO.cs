53fdf3871529ce2129c74abc70fd37f328204e1f
hntduong12345 <hntduong2003@gmail.com>
2024-11-29 12:32:49 +0700

    refactor: comment redundant field in dto

--- InteriorCoffee.Application/DTOs/ChatSession/CreateChatSessionDTO.cs
+++ InteriorCoffee.Application/DTOs/ChatSession/CreateChatSessionDTO.cs
@@ -7,7 +7,7 @@ namespace InteriorCoffee.Application.DTOs.ChatSession
     public class CreateChatSessionDTO
     {
         public List<InteriorCoffee.Domain.Models.Documents.ChatMessage> Messages { get; set; }
-        public DateTime CreatedDate { get; set; }
+        //public DateTime CreatedDate { get; set; }
         public string CustomerId { get; set; } = null!;
         public string AdvisorId { get; set; } = null!;
     }
